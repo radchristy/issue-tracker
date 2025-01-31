e4de1e79f03e40230d4357549c434c53e677f5d5
Christine Hong <christinehong003@gmail.com>
2025-01-31 11:14:18 -0500

    Secure the application

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,34 +1,41 @@
 import { issueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 
 export async function PATCH(
   request: NextRequest,
-  { params }: { params: { id: string } }) { 
-  const body = await request.json()
-  const validation = issueSchema.safeParse(body)
+  { params }: { params: { id: string } }
+) {
+  const session = await getServerSession(authOptions);
+  if (!session) return NextResponse.json({}, { status: 401 });
+  const body = await request.json();
+  const validation = issueSchema.safeParse(body);
   if (!validation.success)
-    return NextResponse.json(validation.error.format(), { status: 400 })
+    return NextResponse.json(validation.error.format(), { status: 400 });
   const issue = await prisma.issue.findUnique({
-    where: {id: parseInt(params.id)}
-  })
+    where: { id: parseInt(params.id) },
+  });
 
   if (!issue)
-    return NextResponse.json({ error: 'Invalid issue' }, { status: 404 })
-  
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
+
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
     data: {
       title: body.title,
-      description: body.description
-    }
-  })
-  return NextResponse.json(updatedIssue)
-  }
+      description: body.description,
+    },
+  });
+  return NextResponse.json(updatedIssue);
+}
 
 export async function DELETE(
   request: NextRequest,
   { params }: { params: { id: string } }) {
+      const session = await getServerSession(authOptions);
+      if (!session) return NextResponse.json({}, { status: 401 });
   const issue = await prisma.issue.findUnique({
     where: {id: parseInt(params.id)}
   })
